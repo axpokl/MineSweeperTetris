16b759fc3efba1ab68719dd8891517a11b9f1063
ax_pokl <ax_pokl@qq.com>
2024-04-05 08:33:53 +0800

    icon

--- make.bat
+++ make.bat
@@ -14,7 +14,7 @@ copy MineSweeperTetris.exe release\MineSweeperTetris.exe
 ::copy steam_appid.txt release\steam_appid.txt
 copy steam_api.dll release\steam_api.dll
 copy disp.dll release\disp.dll
-::copy MineSweeperTetris.ico release\MineSweeperTetris.ico
+copy MineSweeperTetris.ico release\MineSweeperTetris.ico
 xcopy png\* release\png\ /y /r /s
 xcopy wav\* release\wav\ /y /r /s
 xcopy mid\* release\mid\ /y /r /s
